59c44f955a3a0b70d36dfaf19a850d8b7f833a78
Britney Lovell <britneyklovell@gmail.com>
2016-06-28 18:33:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var Express = require('express');
+var app = new Express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var swig = require('swig');
+var path = require('path');
+var routeHandler = require('./routes');
+
+
+// Swig Templeting
+app.engine('html', swig.renderFile); // how to render html templates
+app.set('view engine', 'html'); // what file extension do our templates have
+app.set('views', path.join(__dirname, '/views')); // where to find the views
+swig.setDefaults({ cache: false });
+
+// Static Logging
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, '/public')))
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+
+
+// Error Handling Route
+// catch 404 (i.e., no route was hit) and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// handle all errors (anything passed into `next()`)
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  console.error(err);
+  res.render(
+    'error', { /* FILL THIS IN */ }
+  );
+});
+
+app.listen(function() {
+	console.log('Listening on Port 3000');
+}, 3000);
---
+++ models/index.js
@@ -0,0 +1,76 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost/tripplanner', {
+  logging : true
+});
+
+var Place = db.define('place', {
+	address: {
+		type: Sequelize.STRING
+	},
+	city: {
+		type: Sequelize.STRING
+	},
+	state: {
+		type: Sequelize.STRING
+	},
+	phone: {
+		type: Sequelize.STRING
+	},
+	location: {
+		type: Sequelize.ARRAY(Sequelize.FLOAT)
+	}
+});
+
+var Hotel = db.define('hotel', {
+	name: {
+		type: Sequelize.STRING
+	},
+	numStars: {
+		type: Sequelize.INTEGER,
+		validate: {
+			min: 1,
+			max: 6
+		}
+	},
+	amenities: {
+		type: Sequelize.STRING
+	}
+
+});
+
+var Activity = db.define('activity', {
+	name: {
+		type: Sequelize.STRING
+	},
+	ageRange: {
+		type: Sequelize.STRING		
+	}
+});
+
+var Restaurant = db.define('restaurant', {
+	name: {
+		type: Sequelize.STRING
+	},
+	cuisine: {
+		type: Sequelize.STRING
+	},
+	price: {
+		type: Sequelize.INTEGER
+
+	}
+
+});
+
+var Associations = db.define('associations', {
+	// 
+});
+
+
+
+module.exports = {
+	Place: Place,
+	Hotel: Hotel,
+	Activity: Activity,
+	Restaurant: Restaurant,
+	Associations: Associations
+}
